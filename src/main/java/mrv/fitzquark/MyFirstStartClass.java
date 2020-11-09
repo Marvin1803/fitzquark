9bec6d8d1aa65979b0e8bb58250e8864ac577a48
Marvin Ruppert <MarvinRuppert2@gmx.de>
2020-11-09 13:23:20 +0100

    FitzQuark fertig, juhu

--- src/main/java/mrv/fitzquark/MyFirstStartClass.java
+++ src/main/java/mrv/fitzquark/MyFirstStartClass.java
@@ -3,7 +3,25 @@ package mrv.fitzquark;
 class MyFirstStartClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String fitz = "Fitz";
+        String quark = "Quark";
+        String fitzQuark = "FitzQuark";
+
+        for (int i = 1; i < 101; i++) {
+            if(i%7 == 0 && i%5 == 0)
+            {
+                System.out.println(fitzQuark);
+            }
+            else if (i % 5 == 0)
+            {
+                System.out.println(fitz);
+            }
+            else if (i % 7 == 0)
+            {
+                System.out.println(quark);
+            }
+            else System.out.println(i);
+        }
     }
 
 }
